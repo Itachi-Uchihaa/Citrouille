5f37549709cb82afaf723ecb563b8bd86d6f5fe1
33670 <voldi.zola@wister.fr>
2021-03-12 18:49:38 +0100

    Accueil eleve et professeur creer, ainsi que le dossier auquel apartiendra la navbar

--- Components/Accueil/accueil_Professeur.php
+++ Components/Accueil/accueil_Professeur.php
@@ -1,3 +1,4 @@
+
 <div class="container">
     <div class="content_one">
         <div class="flip-card">
@@ -20,6 +21,19 @@
                 </div>
                 <div class="flip-card-back">
                     <h1 class="h12">LISTE DICTEES</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content_tree">
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img class="" src="\Team-Citrouille\Images\dictee.jpg" alt="Avatar" style="width:300px;height:300px;">
+                </div>
+                <div class="flip-card-back">
+                    <h1 class="h12"> RESULTATS</h1>
                 </div>
             </div>
         </div>
---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Citrouille</title>
+    <link href="index.css" rel="stylesheet">
+    <link href="Components/Accueil/accueil_Eleve.css" rel="stylesheet">
+    <link href="Components/Accueil/accueil_Professeur.css" rel="stylesheet">
+
+</head>
+<body>
+<?php
+//include ("Components/Accueil/accueil_Eleve.php");
+include ("components/Accueil/accueil_Professeur.php");
+?>
+
+
+</body>
+</html>
