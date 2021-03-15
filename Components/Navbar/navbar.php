122efe9a56a64d24bd05aa92658d1f1a59cd58fb
Idriss <idriss.sheick@hotmail.fr>
2021-03-15 20:52:02 +0100

    new navbar

--- Components/Navbar/navbar.php
+++ Components/Navbar/navbar.php
@@ -2,32 +2,27 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="d-flex flex-grow-1">
-        <span class="w-100 d-lg-none d-block">
-            <!-- hidden spacer to center brand on mobile -->
-        </span>
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Dictées</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Mots</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Résultats</a>
-            </li>
-        </ul>
-    </div>
-    <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar7">
-        <ul class="navbar-nav ml-auto flex-nowrap">
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <img src="../../Images/logo.png" width="100em" alt="">
-                </a>
-            </li>
-        </ul>
+        <div>
+            <a class="navbar-brand" href="#" style="padding-left: 1em;">
+                <img src="../../Images/logo.png" width="60em" alt="">
+            </a>
+        </div>
+        <div>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dictées</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mots</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Résultats</a>
+                </li>
+            </ul>
+        </div>
     </div>
     <div>
         <ul class="navbar-nav mr-auto">
--- Components/Resultats/resultats_dictee.php
+++ Components/Resultats/resultats_dictee.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    
+<?php include("../Navbar/navbar.php") ?>
 <?php
 $db_id = 'root';
 $db_utilisateur_password = '';
