ed8754d6620466af30ffc51003b59273c7e1316e
Idriss <idriss.sheick@hotmail.fr>
2021-04-05 17:52:34 +0200

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Citrouille</title>
+</head>
+<body>
+<?php
+include ("Components/Connexion/login.php");
+?>
+</body>
+</html>
