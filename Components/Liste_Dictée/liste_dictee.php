75f45dab7e163535349e9d4b2f737cbb5bcd41f7
VMY <vincent.my@hotmail.fr>
2021-04-05 15:30:04 +0200

    Liste_Mot (Elève)

--- Components/Liste_Dictée/liste_dictee.php
+++ Components/Liste_Dictée/liste_dictee.php
@@ -6,11 +6,9 @@ session_start(); // On démarre la session AVANT toute chose
 
 <head>
   <meta charset="utf-8">
-  <title>Liste Dictée</title>
+  <title>Liste Dictées</title>
   <script src="formapost.js" type="text/javascript"></script>
   <link rel="stylesheet" type="text/css" href="formapost.css">
-  <script src="jquery.js" type="text/javascript"></script>
-  <script src="bootstrap.js" type="text/javascript"></script>
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
@@ -107,7 +105,7 @@ session_start(); // On démarre la session AVANT toute chose
                           <label style="display: block; margin-top: 20px;">Mot '.($i+1).'</label>
                           <input type="text" name="'.$tab_mot_img[0][$i].'" class="field-style field-full align-none" required>
                           <input type="hidden" name="MAX_FILE_SIZE" value="900000000"/>
-                          <input type="file" name="'.$tab_mot_img[1][$i].'"/>
+                          <input type="file" name="'.$tab_mot_img[1][$i].'" required/>
                         </div>
                       ';
                     }
